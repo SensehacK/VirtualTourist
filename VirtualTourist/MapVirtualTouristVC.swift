aa4f2820cc6c5c4073bec28d1adf7a92748a6cf4
SensehacK <kautilyasave@gmail.com>
2016-12-11 12:37:31 +0530

    Added UI , Model & two new files
    Storyboard UI Designed.
    Model Core Data just created.
    Both View Controllers created.
    
    4 errors right now. Still have to decide on the model so connection
    errors are there.

---
+++ VirtualTourist/MapVirtualTouristVC.swift
@@ -0,0 +1,29 @@
+//
+//  MapVirtualTouristVC.swift
+//  VirtualTourist
+//
+//  Created by Sensehack on 11/12/16.
+//  Copyright © 2016 Sensehack. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+import MapKit
+
+class MapVirtualTouristVC : UIViewController , MKMapViewDelegate
+{
+    
+    var userSelectPin
+    
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "seguePhotoViewController" {
+            
+            let controller = segue.destination as! PhotoVirtualTouristVC
+            controller.userSelectPin = userSelectPin
+            
+        }
+    }
+}
---
+++ VirtualTourist/PhotoVirtualTouristVC.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoVirtualTouristVC.swift
+//  VirtualTourist
+//
+//  Created by Sensehack on 11/12/16.
+//  Copyright © 2016 Sensehack. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class PhotoVirtualTouristVC : UIViewController
+{
+    var userSelectPin
+    
+}
