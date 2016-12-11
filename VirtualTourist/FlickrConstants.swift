bd38afc00e2ee626f00037a9eda2e2c45a8ca0ae
SensehacK <kautilyasave@gmail.com>
2016-12-11 12:51:21 +0530

    Added PhotoVC Storyboard UI elements
    Commented other errors right now.
    Will work on Flickr API First , so that it would be easy to grasp the
    Data model Entities / attributes needed next.

---
+++ VirtualTourist/FlickrConstants.swift
@@ -0,0 +1,9 @@
+//
+//  FlickrConstants.swift
+//  VirtualTourist
+//
+//  Created by Sensehack on 11/12/16.
+//  Copyright © 2016 Sensehack. All rights reserved.
+//
+
+import Foundation
---
+++ VirtualTourist/FlickrParseClient.swift
@@ -0,0 +1,9 @@
+//
+//  FlickrParseClient.swift
+//  VirtualTourist
+//
+//  Created by Sensehack on 11/12/16.
+//  Copyright © 2016 Sensehack. All rights reserved.
+//
+
+import Foundation
