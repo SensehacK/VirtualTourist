ca79537348ad6f6a187081d5b927e30fde073df1
SensehacK <kautilyasave@gmail.com>
2016-12-12 13:04:56 +0530

    More Flickr Management & Still Storyboard Wrong Constraints

--- VirtualTourist/FlickrConstants.swift
+++ VirtualTourist/FlickrConstants.swift
@@ -8,9 +8,27 @@
 
 import Foundation
 
-
-
-class FlickrConstants {
+struct FlickrConstants {
+    
+    // Sample URL
+    
+    // URL: https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=6452d34959c669303d0a3b16435aa05d&gallery_id=72157675621648192&format=json&nojsoncallback=1&api_sig=d5be1897030c6877e6be417a47e2cfbe
+    
+    
+    
+    
+    static let APIScheme = "https:"
+    static let APIHost = "api.flickr.com"
+    static let APIPath = "/services/rest"
+    
+    
+    // Latitude & Longitude Square Box Constants
+    static let SearchBBoxHalfWidth = 1.0
+    static let SearchBBoxHalfHeight = 1.0
+    static let SearchLatRange = (-90.0, 90.0)
+    static let SearchLonRange = (-180.0, 180.0)
+    
+    
     
     
     
--- VirtualTourist/FlickrParseClient.swift
+++ VirtualTourist/FlickrParseClient.swift
@@ -9,6 +9,79 @@
 import Foundation
 class FlickrParseClient {
     
+    // MARK: Shared Instance
+    
+    class func sharedInstance() -> FlickrParseClient {
+        struct Singleton {
+            static var sharedInstance = FlickrParseClient()
+        }
+        return Singleton.sharedInstance
+    }
+
+    
+    
+    // Used from "Flick Finder" Jarod Udacity, Created by Jarrod Parkes on 11/5/15.
+    private func bboxString( latitude : Double , longitude : Double ) -> String {
+        
+        
+            let minimumLon = max ( longitude - FlickrConstants.SearchBBoxHalfWidth , FlickrConstants.SearchLonRange.0)
+            let minimumLat = max ( latitude - FlickrConstants.SearchBBoxHalfHeight , FlickrConstants.SearchLatRange.0)
+            let maximumLon = max ( longitude - FlickrConstants.SearchBBoxHalfWidth , FlickrConstants.SearchLonRange.1)
+            let maximumLat = max ( latitude - FlickrConstants.SearchBBoxHalfHeight , FlickrConstants.SearchLatRange.1)
+
+            return "\(minimumLon) , \(minimumLat) , \(maximumLon) , \(maximumLat) "
+        
+        if (latitude == 0.0 && longitude == 0.0) {
+            return "0,0,0,0"
+        }
+    }
+    
+    
+    
+    
+    
+    // Build URL & return the request.
+    
+    func getBuildURL(parameters : [String:AnyObject]) -> URLRequest {
+        
+        // Building URL Components
+        var components = URLComponents()
+        components.scheme = FlickrConstants.APIScheme
+        components.host   = FlickrConstants.APIHost
+        components.path   = FlickrConstants.APIPath
+        
+        components.queryItems = [URLQueryItem]()
+        
+        for (keys , values) in parameters {
+            let queryItem = URLQueryItem(name: keys, value: values as? String)
+            components.queryItems?.append(queryItem)
+        }
+        
+        if  components.url == nil
+        {
+            print("Error in URL Creation")
+        }
+        
+        guard let urlrequested = components.url else {
+            print("Error in URL Creation")
+            let url2 = NSURL(string: "https://www.flickr.com/photos/flickr/30709520093/in/feed")
+            let url = URLRequest.init(url: url2 as! URL)
+            return url
+        }
+        
+        
+        let request = URLRequest(url: urlrequested)
+        return request
+        
+        
+    }
+    
+    
+    //
+    
+    
+    
+    
     
     
 }
