544029a4530a256de94042b7716fef858200ff1a
SensehacK <kautilyasave@gmail.com>
2016-12-14 12:30:48 +0530

    Created PhotoCellVT & also linked them from Storyboard

---
+++ VirtualTourist/PhotoCellVT.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoCellVT.swift
+//  VirtualTourist
+//
+//  Created by Sensehack on 14/12/16.
+//  Copyright © 2016 Sensehack. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PhotoCellVT : UICollectionViewCell {
+    
+    @IBOutlet weak var imageViewCell: UIImageView!
+    
+    @IBOutlet weak var activityIndicatorInCell: UIActivityIndicatorView!
+    
+    
+}
